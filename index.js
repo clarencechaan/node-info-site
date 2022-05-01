187295dc205eb455ec21cf3e0b9f733703c70c18
Clarence Chan <clarencechaan@icloud.com>
2022-05-01 14:59:04 -0400

    basic http server

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    console.log(req.url);
+    if (req.url === "/") {
+      html = fs.readFileSync("index.html");
+    } else if (req.url === "/about") {
+      html = fs.readFileSync("about.html");
+    } else if (req.url === "/contact-me") {
+      html = fs.readFileSync("contact-me.html");
+    } else {
+      html = fs.readFileSync("404.html");
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    if (html) {
+      res.end(html);
+    }
+  })
+  .listen(8080);
